6c16fb84bd2afe7b1c7c66b5591430ec08fd134b
minici <kim.sungmin@nhn.com>
2013-03-29 11:51:15 +0900

    initial commit
    Signed-off-by: minici <kim.sungmin@nhn.com>

---
+++ git/src/com/nhncorp/git/TestMain.java
@@ -0,0 +1,13 @@
+package com.nhncorp.git;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
